7dd33c4b4627034a9f15f8519aeaaa14b76a12ef
Mariano Villarreal <marianov994@gmail.com>
2020-11-09 01:24:39 -0300

    new country view

--- src/components/view/Country.tsx
+++ src/components/view/Country.tsx
@@ -2,11 +2,13 @@ import React, { FunctionComponent, Fragment } from 'react';
 
 import IconAdd from './../../assets/svg/add.svg';
 
+import ListItems from './../smart/ListItems/ListItems';
 import ScreenContainer from '../dumb/ScreenContainer/ScreenContainer';
 import HeaderContainer from '../dumb/HeaderContainer/HeaderContainer';
 import Menu from '../smart/Menu/Menu';
 import StatusHeader from '../smart/StatusHeader/StatusHeader';
 import CardHeader from '../dumb/CardHeader/CardHeader';
+import spain from './../../assets/españa.jpg'
 
 const Country: FunctionComponent = () => {
   // const state = useSelector((state: any) => state);
@@ -17,17 +19,39 @@ const Country: FunctionComponent = () => {
 
   return (
     <Fragment>
-      <HeaderContainer />
-      <StatusHeader />
-      <ScreenContainer>
-        <CardHeader
-          content="Australia"
-          subtitle="Paso 1"
-          icon={IconAdd}
-          className="header" 
-        />
-        <Menu />
+      <img style={{ 
+        width: "100%",
+        marginBottom: "-25%"
+      }} src={spain} />
+      <ScreenContainer className="white">
+        <StatusHeader />
+          <ListItems
+            items={
+              [{
+                operation_type: "add",
+                date: "4",
+                currencyEnd: "2",
+                currencyStart: "3",
+                ingressAmount: "5",
+                substractionAmount: "6",
+                trading_type: "7",
+              }, {
+                operation_type: "add",
+                date: "4",
+                currencyEnd: "2",
+                currencyStart: "3",
+                ingressAmount: "5",
+                substractionAmount: "6",
+                trading_type: "7",
+              }]
+            }
+            includeSpan
+            onClickSpan={() => true}
+          />
       </ScreenContainer>
+      {/* <ScreenContainer>
+        <Menu />
+      </ScreenContainer> */}
     </Fragment>
   );
 };
